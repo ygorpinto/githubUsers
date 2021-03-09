ed4485c0fd28750451c61ec52b072c472ecf7b17
Ygor Ferreira <ygor.pinto@hotmail.com>
2021-03-09 19:56:17 -0300

    ok

---
+++ styles/dark.ts
@@ -0,0 +1,9 @@
+export default {
+    title: "dark",
+    colors: {
+        background:"",
+        title:"",
+        text:"",
+
+    }
+};
---
+++ styles/light.ts
@@ -0,0 +1,9 @@
+export default {
+    title: "light",
+    colors: {
+        background:"",
+        title:"",
+        text:"",
+
+    }
+};
