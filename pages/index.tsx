1e3a84732cc3733318f318d9061140c14329b6a4
Ygor Ferreira <ygor.pinto@hotmail.com>
2021-03-09 11:26:50 -0300

    auth ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
-  return (
-    <div>
-      oi
-    </div>
-  )
+import { signIn, signOut, useSession } from 'next-auth/client'
+
+export default function Login() {
+  const [ session, loading ] = useSession()
+
+  return <>
+    {!session && <div className="notSignIn">
+      Not signed in <br/>
+      <button onClick={() => signIn()}>Sign in</button>
+    </div>}
+    {session && <div className="signIn">
+      Signed in as {session.user.email} <br/>
+      <button onClick={() => signOut()}>Sign out</button>
+    </div>}
+  </>
 }
